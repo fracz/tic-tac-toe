b809b75e512b2cd336e20535afdc44373eee4dd4
fracz <fraczwojciech@gmail.com>
2014-05-05 20:33:29 +0200

    Fix visualization error

--- data/src/Visualizer.php
+++ data/src/Visualizer.php
@@ -6,7 +6,7 @@ require_once 'WinChecker.php';
 class Visualizer extends Stage{
 	public function render(){
 		$r = PHP_EOL;
-		$r .= $this->state[0] . $this->state[2] . $this->state[2] . PHP_EOL;
+		$r .= $this->state[0] . $this->state[1] . $this->state[2] . PHP_EOL;
 		$r .= $this->state[3] . $this->state[4] . $this->state[5] . PHP_EOL;
 		$r .= $this->state[6] . $this->state[7] . $this->state[8] . PHP_EOL;
 		$wc = new WinChecker($this->state);
