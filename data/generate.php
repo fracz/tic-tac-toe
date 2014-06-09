c59e2a8530f190170384c6ee5ba7a3d8305d09fd
fracz <fraczwojciech@gmail.com>
2014-06-09 12:17:52 +0200

    More clever data

--- data/generate.php
+++ data/generate.php
@@ -27,33 +27,59 @@
         if(!trim($line)) continue;
         $winChecker = new WinChecker($line);
         if($winChecker->xWins() || $winChecker->oWins()) continue;
-        $stage = explode(',', trim($line));
-        $bestMove = 0;
-        $bestSupport = -1;
-        for($i = 0; $i < 9; $i++){
-            if($stage[$i] != NONE) continue;
-            $stage[$i] = X;
-            $support = 0;
-            foreach($xWins as $xWin){
-                $match = true;
-                for($j = 0; $j < 9; $j++){
-                    if($stage[$j] != NONE && $stage[$j] != $xWin[$j]){
-                        $match = false;
-                        break;
-                    }
-                }
-                if($match) ++ $support;
-            }
-            if($support > $bestSupport) {
-                $bestSupport = $support;
-                $bestMove = $i;
-            }
-            $stage[$i] = NONE;
-        }
+		$stage = explode(',', trim($line));
 		$counts = array_count_values($stage);
 		if(!isset($counts[O]))$counts[O] = 0;
 		if(!isset($counts[X]))$counts[X] = 0;
-		$kalaFix = abs($counts[O] - $counts[X]) <= 1;
-        if($bestSupport >= 0 && $kalaFix)
+		$diff = $counts[O] - $counts[X];
+		$kalaFix = $diff <= 1 && $diff >= 0;
+		if(!$kalaFix) continue;
+		
+        $bestMove = -1;
+        $bestSupport = -1;
+		for($i = 0; $i < 9; $i++){
+            if($stage[$i] != NONE) continue;
+            $stage[$i] = X;
+			$winChecker = new WinChecker($stage);
+			if($winChecker->xWins()){
+				$bestMove = $i;
+			}
+			$stage[$i] = NONE;
+		}
+		if($bestMove < 0){
+			for($i = 0; $i < 9; $i++){
+				if($stage[$i] != NONE) continue;
+				$stage[$i] = O;
+				$winChecker = new WinChecker($stage);
+				if($winChecker->oWins()){
+					$bestMove = $i;					
+				}
+				$stage[$i] = NONE;
+			}
+		}
+		if($bestMove < 0){
+			for($i = 0; $i < 9; $i++){
+				if($stage[$i] != NONE) continue;
+				$stage[$i] = X;
+				$support = 0;
+				foreach($xWins as $xWin){
+					$match = true;
+					for($j = 0; $j < 9; $j++){
+						if($stage[$j] != NONE && $stage[$j] != $xWin[$j]){
+							$match = false;
+							break;
+						}
+					}
+					if($match) ++ $support;
+				}
+				if($support > $bestSupport) {
+					$bestSupport = $support;
+					$bestMove = $i;
+				}
+				$stage[$i] = NONE;
+			}
+		}
+		
+        if($bestMove >= 0)
             echo implode(',', $stage) . ',' . $bestMove . PHP_EOL;
     }
